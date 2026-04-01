50d5e01367ad1c45e1aae157f0bedce920103e23
Дмитрий <965910@bk.ru>
2026-04-02 01:13:34 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,42 @@
+const CACHE_NAME = 'scm-news-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/about.html',
+  '/expertise.html',
+  '/experience.html',
+  '/trends.html',
+  '/contact.html',
+  '/data/news.json',
+  '/rss.xml',
+  '/sitemap.xml',
+  '/robots.txt',
+  '/1favicon.ico',
+  '/js/subscribe.js',
+  '/js/analytics.js',
+  '/js/register-sw.js',
+  '/icons/android/launchericon-192x192.png',
+  '/icons/android/launchericon-512x512.png'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+});
