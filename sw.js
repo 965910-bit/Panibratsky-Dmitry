4993384052cb664a98e43cef4a9d4dae2f713d4b
Дмитрий <965910@bk.ru>
2026-04-02 04:16:07 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,23 @@
 const CACHE_NAME = 'scm-news-v1';
 const urlsToCache = [
-  '.',
-  'index.html',
-  'about.html',
-  'expertise.html',
-  'experience.html',
-  'trends.html',
-  'contact.html',
-  'data/news.json',
-  'rss.xml',
-  'sitemap.xml',
-  'robots.txt',
-  '1favicon.ico',
-  'js/subscribe.js',
-  'js/analytics.js',
-  'js/register-sw.js',
-  'icons/android/launchericon-192x192.png',
-  'icons/android/launchericon-512x512.png'
+  '/Panibratsky-Dmitry/',
+  '/Panibratsky-Dmitry/index.html',
+  '/Panibratsky-Dmitry/about.html',
+  '/Panibratsky-Dmitry/expertise.html',
+  '/Panibratsky-Dmitry/experience.html',
+  '/Panibratsky-Dmitry/trends.html',
+  '/Panibratsky-Dmitry/contact.html',
+  '/Panibratsky-Dmitry/data/news.json',
+  '/Panibratsky-Dmitry/rss.xml',
+  '/Panibratsky-Dmitry/sitemap.xml',
+  '/Panibratsky-Dmitry/robots.txt',
+  '/Panibratsky-Dmitry/1favicon.ico',
+  '/Panibratsky-Dmitry/js/subscribe.js',
+  '/Panibratsky-Dmitry/js/analytics.js',
+  '/Panibratsky-Dmitry/js/register-sw.js',
+  '/Panibratsky-Dmitry/js/install-prompt.js',
+  '/Panibratsky-Dmitry/icons/android/launchericon-192x192.png',
+  '/Panibratsky-Dmitry/icons/android/launchericon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -26,9 +27,24 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  const url = new URL(event.request.url);
+  if (url.pathname.includes('/data/news.json')) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => response || fetch(event.request))
+    );
+  }
 });
 
 self.addEventListener('activate', event => {
